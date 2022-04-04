25e7952d235296219d2efbe66a27fef366cf0243
ricardo3826 <48776473+ricardo3826@users.noreply.github.com>
2022-04-04 00:59:56 -0400

    Limpiando el codigo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import generateCard from './generateCard'   
-import './styles/main.scss'
+import generateCard from "./generateCard";
+import "./styles/main.scss";
 //import imgSmiling from './assets/SmilingFriendsTitleCard.png'
 
+const CardBtn = document.getElementById("Cardbtn");
+CardBtn.addEventListener("click", generateCard);
 
-
-
-
-const CardBtn = document.getElementById('Cardbtn')
-CardBtn.addEventListener('click', generateCard)
-
-
-generateCard()
+generateCard();
